b41ad3e3df867b357813a072404a64d4141afbe6
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-03-04 01:12:56 -0800

    Completed on Location Service, Privacy as well!

--- Pixel-City/Controllers/MapVC.swift
+++ Pixel-City/Controllers/MapVC.swift
@@ -8,21 +8,36 @@
 
 import UIKit
 import MapKit
+import CoreLocation
 
 class MapVC: UIViewController {
 
     @IBOutlet weak var mapView: MKMapView!
     
+    //Location Service
+    var locationManager = CLLocationManager()
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    
+    //location Constant
+    let regionRadius: Double = 1000
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         mapView.delegate = self
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        self.mapView.showsUserLocation = true
+        configureLocationServices()
     }
 
     
     
     @IBAction func centerMapBtnWasPressed(_ sender: Any) {
-        
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
     
     
@@ -33,7 +48,25 @@ class MapVC: UIViewController {
     
 }
 
-
+// Set Location service Radius on User location
 extension MapVC: MKMapViewDelegate {
-    
+    func centerMapOnUserLocation() {
+        guard let cordinate = locationManager.location?.coordinate else {return}
+        let cordinateRegion = MKCoordinateRegionMakeWithDistance(cordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(cordinateRegion, animated: true)
+    }
+}
+
+//LocationService Extn.
+extension MapVC: CLLocationManagerDelegate {
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
 }

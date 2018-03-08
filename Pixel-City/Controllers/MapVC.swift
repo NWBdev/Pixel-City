211099566a009e46938597afc02b6a88e40a5567
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-03-07 21:27:43 -0800

    Completed loading images from Flickr API to CollectionView

--- Pixel-City/Controllers/MapVC.swift
+++ Pixel-City/Controllers/MapVC.swift
@@ -52,7 +52,7 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         collectionView?.register(PhotoCell.self, forCellWithReuseIdentifier: "PhotoCell")
         collectionView?.delegate = self
         collectionView?.dataSource = self
-        collectionView?.backgroundColor = #colorLiteral(red: 0.4666666687, green: 0.7647058964, blue: 0.2666666806, alpha: 1) // test
+        collectionView?.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1) // test
         
         pullUpView.addSubview(collectionView!)
     }
@@ -167,6 +167,13 @@ extension MapVC: MKMapViewDelegate {
         removeProgressLbl()
         cancelAllSessions()
         
+        //reload and clear previous Image Array
+        imageUrlArray = []
+        imageArray = []
+        
+        //add data of images
+        collectionView?.reloadData()
+        
         //pulls up view for Images
         animateViewUp()
         //Pull down View
@@ -203,6 +210,7 @@ extension MapVC: MKMapViewDelegate {
                         //hide label
                         self.removeProgressLbl()
                         //reload CollectionView
+                        self.collectionView?.reloadData()
                     }
                 })
             }
@@ -218,8 +226,6 @@ extension MapVC: MKMapViewDelegate {
     
     //Alamofire Request
     func retrieveUrls(forAnnotation annotation: DroppablePin, handler: @escaping (_ status: Bool) ->() ){
-        //empty array for every Intance
-        imageUrlArray = []
         
         Alamofire.request(flickrUrl(forApiKey: apiKey, withAnnotation: annotation, andNumberOfPhotos: 40)).responseJSON { (response) in
             guard let json = response.result.value as? Dictionary<String, AnyObject> else {return}
@@ -235,8 +241,6 @@ extension MapVC: MKMapViewDelegate {
     }
     
     func retrieveImages(handler: @escaping (_ status: Bool) -> ()) {
-        imageArray = []
-        
         for url in imageUrlArray {
             Alamofire.request(url).responseImage(completionHandler: { (response) in
                 guard let image = response.result.value else {return}
@@ -292,13 +296,15 @@ extension MapVC: UICollectionViewDelegate, UICollectionViewDataSource {
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // number of Items in array
-        return 4
+        return imageArray.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell
-        
-        return cell!
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PhotoCell", for: indexPath) as? PhotoCell else { return UICollectionViewCell() }
+        let imageFromIndex = imageArray[indexPath.row]
+        let imageView = UIImageView(image: imageFromIndex)
+        cell.addSubview(imageView)
+        return cell
     }
 }
 

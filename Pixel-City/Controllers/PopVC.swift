9628f60bf86fa491c553f710cfa0ab6221f98201
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-03-09 01:13:05 -0800

    Added PopVC, and double tap UIGestureRecognizer for dismissing PopVC when user is done viewing Image.

---
+++ Pixel-City/Controllers/PopVC.swift
@@ -0,0 +1,61 @@
+//
+//  PopVC.swift
+//  Pixel-City
+//
+//  Created by Nathaniel Burciaga on 3/9/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+class PopVC: UIViewController, UIGestureRecognizerDelegate {
+    //Outlets
+    @IBOutlet weak var popImageView: UIImageView!
+    
+    //Variables
+    var passedImage: UIImage!
+    
+    
+    
+    func initData(forImage image: UIImage) {
+        self.passedImage = image
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        popImageView.image = passedImage
+        //dismiss PopView
+        addDoubleTap()
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    func addDoubleTap() {
+        let doubleTap =  UITapGestureRecognizer(target: self, action: #selector(screenWasDoubleTapped))
+        doubleTap.numberOfTapsRequired = 2
+        doubleTap.delegate = self
+        view.addGestureRecognizer(doubleTap)
+    }
+    
+    
+    @objc func screenWasDoubleTapped() {
+        dismiss(animated: true, completion: nil)
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

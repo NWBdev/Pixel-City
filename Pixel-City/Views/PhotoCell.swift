7ad65e9bf7a4feed13958539eced0ae98ff8fed6
Nathaniel Burciaga <nathanielburciaga25@gmail.com>
2018-03-11 23:28:27 -0700

    added 3D touch & PopVC to UICollectionView

---
+++ Pixel-City/Views/PhotoCell.swift
@@ -0,0 +1,19 @@
+//
+//  photoCell.swift
+//  Pixel-City
+//
+//  Created by Nathaniel Burciaga on 3/5/18.
+//  Copyright © 2018 Nathaniel Burciaga. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
